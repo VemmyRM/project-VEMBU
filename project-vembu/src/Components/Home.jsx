3b8bd8a2ebf44b1cf719f62490c192be34452ebe
VemmyRM <kwvembu@gmail.com>
2021-05-29 20:28:42 -0400

    adding style changes

--- project-vembu/src/Components/Home.jsx
+++ project-vembu/src/Components/Home.jsx
@@ -7,18 +7,18 @@ const homePage = () => {
         <div>
             <Navbar />
             <center>
-                <div className="welcome container">
-                    <div>
-                        <div>
-                            <h1>Welcome to the SchoolFinder!</h1>
-                            <h2>Where would you like to go?</h2>
-                            <div>
-                                <Link className="btn btn-lg btn-primary home-btn" to="/create">Create</Link>
-                                <Link className="btn btn-lg btn-primary home-btn" to="/browse">Browse</Link>
+                <div style={{ "zIndex": "20", "textAlign": "left" }}>
+                    <img src="https://dreamschools.com/static/media/mainheroimg.59aec240.png" style={{ "position": "absolute", "right": "50px", "zIndex": "-1", "maxWidth": "50%" }} />
+                    <div className="welcome container">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <h1>Welcome to the SchoolFinder!</h1>
+                                <h2>Where would you like to go?</h2>
+                                <div>
+                                    <Link className="btn btn-lg btn-primary home-btn" to="/create">Create</Link>
+                                    <Link className="btn btn-lg btn-primary home-btn" to="/browse">Browse</Link>
+                                </div>
                             </div>
-                        </div>
-                        <div className="mt-5">
-                            <img src="https://dreamschools.com/static/media/mainheroimg.59aec240.png" />
                         </div>
                     </div>
                 </div>
